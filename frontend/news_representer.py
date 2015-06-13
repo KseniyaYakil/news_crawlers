f57c7ff9abf394fe9d17f5d812da12611e171718
Kseniya Yakil <k.yakil@yandex.ru>
2015-06-13 17:14:20 +0300

    - auth/login/register/logout pages were added

--- frontend/news_representer.py
+++ frontend/news_representer.py
@@ -6,7 +6,7 @@ from db_connector import DBConnector
 import time
 import datetime
 
-class MainHandler(tornado.web.RequestHandler):
+class AllNewsHandler(tornado.web.RequestHandler):
 	def __get_prev_days__(self, n_days):
 		prev_day = datetime.datetime.now() - datetime.timedelta(days=n_days)
 		prev_date_str = prev_day.strftime("%Y-%m-%d %H:%M:%S")
@@ -17,7 +17,7 @@ class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		db_conn = DBConnector()
 
-		after_time = self.__get_prev_days__(1)
+		after_time = self.__get_prev_days__(60)
 		news_items = db_conn.after_date_news(after_time)
 
 		to_render = []
@@ -30,8 +30,90 @@ class IDHandler(tornado.web.RequestHandler):
 	def get(self, id_item):
 		self.write("requested news item with id = " + id_item)
 
+class LoginHandler(tornado.web.RequestHandler):
+	def get(self):
+		if self.current_user:
+			#TODO: to template
+			self.write("You are already logged in")
+			return
+		self.render("login.html")
+
+	def post(self):
+		username = self.get_argument('username', '')
+		password = self.get_argument('password', '')
+
+		if not username or not password:
+			self.redirect("/login")
+			return
+		self.write("your username {} and password {}".format(username, password))
+
+class LogoutHandler(tornado.web.RequestHandler):
+	def get(self):
+		if not self.current_user:
+			self.redirect("/")
+		self.render("logout.html")
+
+	def post(self):
+		if not self.current_user:
+			self.redirect("/")
+
+		# TODO: log out there
+		self.write("Log out there")
+
+
+class RegisterHandler(tornado.web.RequestHandler):
+	def get(self):
+		if self.current_user:
+			#TODO: to template
+			self.write("You are already logged in")
+			return
+		self.render("register.html")
+
+	def post(self):
+		user_data = {}
+		user_data['name'] = self.get_argument('name', '')
+		user_data['surname'] = self.get_argument('surname', '')
+		user_data['username'] = self.get_argument('username', '')
+		user_data['password'] = self.get_argument('password', '')
+
+		for k, v in user_data.items():
+			if not v:
+				self.redirect("/register")
+				return
+
+		self.write("you reg data {}".format(user_data))
+
+class AuthHandler(tornado.web.RequestHandler):
+	def get(self):
+		if self.current_user:
+			self.redirect("/")
+			return
+		self.render("auth.html")
+
+	def post(self):
+		if self.get_argument('login', '') != '':
+			self.redirect("/login")
+			return
+		if self.get_argument('register', '') != '':
+			self.redirect("/register")
+			return
+		raise tornado.web.HTTPError(500)
+
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		if not self.current_user:
+			self.redirect("/auth")
+			return
+		# TODO: foreach user role show different home pages
+		self.write("Welcome to news rating system!")
+
 application = tornado.web.Application([
 	(r"/", MainHandler),
+	(r"/auth", AuthHandler),
+	(r"/login", LoginHandler),
+	(r"/logout", LogoutHandler),
+	(r"/register", RegisterHandler),
+	(r"/all_news", AllNewsHandler),
 	(r"/id/([0-9]+)", IDHandler)
 ])
 
