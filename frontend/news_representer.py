44f80ad16d1e8b0fd569bfd0b26f11551ccb34d1
Kseniya Yakil <k.yakil@yandex.ru>
2015-06-25 08:03:55 +0300

    - use simple bootstrap

--- frontend/news_representer.py
+++ frontend/news_representer.py
@@ -15,37 +15,13 @@ import datetime
 expert_main = 'http://127.0.0.1:8890'
 researcher_main = 'http://127.0.0.1:8891'
 
-class AllNewsHandler(tornado.web.RequestHandler):
-	def __get_prev_days__(self, n_days):
-		prev_day = datetime.datetime.now() - datetime.timedelta(days=n_days)
-		prev_date_str = prev_day.strftime("%Y-%m-%d %H:%M:%S")
-		prev_time_tuple = time.strptime(prev_date_str, "%Y-%m-%d %H:%M:%S")
-
-		return time.gmtime(time.mktime(prev_time_tuple))
-
-	def get(self):
-		db_conn = DBConnector()
-
-		after_time = self.__get_prev_days__(15)
-		news_items = db_conn.after_date_news(after_time)
-
-		to_render = []
-		for n in news_items:
-			to_render.append("Title: {} \nSummary: {}".format(n['title'], n.get('summary', "none")))
-
-		self.render("all_news.html", title="recent news", items=to_render)
-
-class IDHandler(tornado.web.RequestHandler):
-	def get(self, id_item):
-		self.write("requested news item with id = " + id_item)
-#---------------------------------------------------------------
-
 class LoginHandler(BaseHandler):
 	def get(self):
 		if self.get_current_user():
 			self.redirect("/logout")
 			return
-		self.render("login.html")
+		self.render("login_bootstrap.html")
+		#self.render("login.html")
 
 	def post(self):
 		user_data = {}
@@ -75,7 +51,7 @@ class LogoutHandler(BaseHandler):
 			self.redirect("/")
 			return
 
-		self.render("logout.html")
+		self.render("logout_bootstrap.html")
 
 	def post(self):
 		user_cookie = self.get_current_user()
@@ -93,7 +69,7 @@ class RegisterHandler(BaseHandler):
 		if self.get_current_user():
 			self.redirect("/logout")
 			return
-		self.render("register.html")
+		self.render("register_bootstrap.html")
 
 	def post(self):
 		user_data = {}
@@ -114,14 +90,15 @@ class RegisterHandler(BaseHandler):
 		msg = 'Successfull registration'
 		if resp.status != 201:
 			msg = resp.reason
-		self.render("end_of_user_reg.html", msg=msg)
+		self.render("end_of_user_reg_bootstrap.html", msg=msg)
 
 class AuthHandler(BaseHandler):
 	def get(self):
 		if self.get_current_user():
 			self.redirect("/")
 			return
-		self.render("auth.html")
+		self.render("auth_bootstrap.html")
+		#self.render("auth.html")
 
 	def post(self):
 		if self.get_argument('login', '') != '':
@@ -130,6 +107,7 @@ class AuthHandler(BaseHandler):
 		if self.get_argument('register', '') != '':
 			self.redirect("/register")
 			return
+		print 'here'
 		raise tornado.web.HTTPError(500)
 
 class MainHandler(BaseHandler):
@@ -152,14 +130,24 @@ class MainHandler(BaseHandler):
 		else:
 			self.write("Welcome to news rating system!")
 
+
 application = tornado.web.Application([
 	(r"/", MainHandler),
 	(r"/auth", AuthHandler),
 	(r"/login", LoginHandler),
 	(r"/logout", LogoutHandler),
 	(r"/register", RegisterHandler),
-	(r"/all_news", AllNewsHandler),
-	(r"/id/([0-9]+)", IDHandler)
+	(r"/css/bootstrap\.css()", tornado.web.StaticFileHandler, {'path': '../static/css/bootstrap.css'}),
+	(r"/js/bootstrap.min.js()", tornado.web.StaticFileHandler, {'path': '../static/js/bootstrap.min.js'}),
+	(r"/index_files/cloudflare.js()", tornado.web.StaticFileHandler, {'path': '../static/index_files/cloudflare.js'}),
+	(r"/index_files/ga.js()", tornado.web.StaticFileHandler, {'path': '../static/index_files/ga.js'}),
+	(r"/index_files/rocket.js()", tornado.web.StaticFileHandler, {'path': '../static/index_files/rocket.js'}),
+	(r"/index_files/bootstrap-responsive.css()", tornado.web.StaticFileHandler, {'path': '../static/index_files/bootstrap-responsive.css'}),
+	(r"/index_files/bootstrap.css()", tornado.web.StaticFileHandler, {'path': '../static/index_files/bootstrap.css'}),
+	(r"/index_files/rocket.js()", tornado.web.StaticFileHandler, {'path': '../static/index_files/rocket.js'}),
+#	(r"/()", tornado.web.StaticFileHandler, {'path': '../static/}'),
+#	(r"/()", tornado.web.StaticFileHandler, {'path': '../static/}'),
+#	(r"/()", tornado.web.StaticFileHandler, {'path': '../static/}'),
 ])
 
 if __name__ == "__main__":
